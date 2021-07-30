035f734244e0bf87ac728d5e277f5a46dcb241b6
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2021-07-30 17:45:00 +0530

    Built up the SRT file using datetime, wave, contextlib module.

---
+++ task_1_using_wave.py
@@ -0,0 +1,72 @@
+import wave, math, contextlib
+import speech_recognition as sr
+import moviepy.editor as mp
+
+import datetime
+
+transcribed_audio_file_name = "output/task_audio.wav"
+video_file_name = "task_video.mp4"
+
+
+# audioclip = AudioFileClip(video_file_name)
+
+audioclip = mp.VideoFileClip(video_file_name)
+
+audioclip.audio.write_audiofile(transcribed_audio_file_name)
+
+# for finding the total length of the audio file
+with contextlib.closing(wave.open(transcribed_audio_file_name,'r')) as f:
+    frames = f.getnframes()
+    rate = f.getframerate()
+    duration = frames / float(rate)
+    
+total_segments = math.ceil(duration/10)
+
+r = sr.Recognizer()
+i = 0
+start_time = datetime.datetime(2021,7,30,0,0,0)
+current_time = start_time
+end_time = 0
+time_add = 10
+
+#building SRT file
+#another file is also being made for simple transcripted text
+
+f1 = open("output/transcription_using_wave.txt", 'w')
+f2 = open("output/srt_file.srt",'w')
+
+for i in range(0, total_segments):
+    with sr.AudioFile(transcribed_audio_file_name) as source:
+        audio = r.record(source, offset=i*10, duration=10)                #subclip function can also be used to divide the audio into segments
+    
+    text = r.recognize_google(audio)
+    
+    f1.write(text)
+    f1.write("\n\n")
+
+    if i == 0:
+        current_time = start_time
+        end_time = current_time + datetime.timedelta(0,time_add)
+
+    if i > 0:
+        current_time = end_time
+        end_time = current_time + datetime.timedelta(0,time_add)
+    
+    str_current_time = str(current_time.time())
+    str_end_time = str(end_time.time())
+    
+    f2.write(str(i+1))
+    f2.write("\n")
+    f2.write(str_current_time)
+    f2.write(" --> ")
+    f2.write(str_end_time)
+    f2.write("\n")
+    f2.write(text)
+    f2.write("\n\n")
+    
+f1.close()
+f2.close()
+
+print("\n\nHey, It's done. Check your files in the output folder.")
+
+
