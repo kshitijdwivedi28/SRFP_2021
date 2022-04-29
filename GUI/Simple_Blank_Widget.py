a8bbd686959296dcf6cde82d0c621b01fb2d3964
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-04-29 22:31:15 +0530

    Create Simple_Blank_Widget.py

---
+++ GUI/Simple_Blank_Widget.py
@@ -0,0 +1,14 @@
+# importing module
+from tkinter import *
+
+# creating object 
+root = Tk()
+
+# Creating Lable Widget
+myLabel = Label(root, text = "SRFP PROJECT - SUBTITLE (SRT) GENERATOR")
+
+# Putting it up on screen
+myLabel.pack()
+
+# Hovering on the screen
+root.mainloop()
