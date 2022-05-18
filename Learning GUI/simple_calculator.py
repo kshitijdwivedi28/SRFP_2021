6c3eb74a554a95cc301da666bf112883a2a4db52
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-05-18 16:36:16 +0530

    added more functionality

--- Learning GUI/simple_calculator.py
+++ Learning GUI/simple_calculator.py
@@ -1,5 +1,7 @@
 import tkinter
 
+from pyparsing import opAssoc
+
 root = tkinter.Tk()
 root.title("SIMPLE CALCULATOR")
 
@@ -8,6 +10,8 @@ enter_data = tkinter.Entry(root, width = 40, borderwidth = 5)
 enter_data.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
 
 num1 = 0
+num2 = 0
+op = ""
  
 def inp_btn(num):
     curr_inp = enter_data.get()
@@ -22,12 +26,46 @@ def add_btn():
     global num1 
     num1 = enter_data.get()
     enter_data.delete(0, tkinter.END)
+    global op
+    op = "+"
+    
+def sub_btn():
+    global num1 
+    num1 = enter_data.get()
+    enter_data.delete(0, tkinter.END)
+    global op
+    op = "-"
+    
+def mul_btn():
+    global num1 
+    num1 = enter_data.get()
+    enter_data.delete(0, tkinter.END)
+    global op
+    op = "*"
+    
+
+def div_btn():
+    global num1 
+    num1 = enter_data.get()
+    enter_data.delete(0, tkinter.END)
+    global op
+    op = "/"
     
     
 def res_btn():
     num2 = enter_data.get()
     enter_data.delete(0, tkinter.END)
-    enter_data.insert(0, (int(num1) + int(num2)))
+
+    if op == "+" :
+        enter_data.insert(0, (int(num1) + int(num2)))
+    elif op == "-" :
+        enter_data.insert(0, (int(num1) - int(num2)))
+    elif op == "*" :
+        enter_data.insert(0, (int(num1) * int(num2)))
+    elif op == "/" :
+        enter_data.insert(0, (int(int(num1) / int(num2))))
+        
+    
     
 
 # Creating buttons
@@ -41,9 +79,18 @@ btn_7 = tkinter.Button(root, text = "7", padx = 40, pady = 20, command = lambda 
 btn_8 = tkinter.Button(root, text = "8", padx = 40, pady = 20, command = lambda : inp_btn(8))
 btn_9 = tkinter.Button(root, text = "9", padx = 40, pady = 20, command = lambda : inp_btn(9))
 btn_0 = tkinter.Button(root, text = "0", padx = 40, pady = 20, command = lambda : inp_btn(0))
-btn_clr = tkinter.Button(root, text = "CLR", padx = 32.5, pady = 20, command = clr_btn)
+
 btn_plus = tkinter.Button(root, text = "+", padx = 40, pady = 20, command = add_btn)
-btn_res = tkinter.Button(root, text = "=", padx = 135, pady = 20, command = res_btn)
+btn_sub = tkinter.Button(root, text = "-", padx = 40, pady = 20, command = sub_btn)
+btn_mul = tkinter.Button(root, text = "x", padx = 40, pady = 20, command = mul_btn)
+btn_div = tkinter.Button(root, text = "/", padx = 40, pady = 20, command = div_btn)
+
+
+btn_res = tkinter.Button(root, text = "RES", padx = 32.5, pady = 20, command = res_btn)
+btn_clr = tkinter.Button(root, text = "CLR", padx = 32.5, pady = 20, command = clr_btn)
+
+
+
 
 # Putting them on screen
 btn_1.grid(row = 3, column = 0)
@@ -59,10 +106,14 @@ btn_8.grid(row = 1, column = 1)
 btn_9.grid(row = 1, column = 2)
 
 btn_0.grid(row = 4, column = 1)
-btn_clr.grid(row = 4, column = 0)
-btn_plus.grid(row = 4, column = 2)
 
-btn_res.grid(row = 5, column = 0, columnspan = 3)
+btn_plus.grid(row = 5, column = 0, columnspan = 2)
+btn_sub.grid(row = 5, column = 1, columnspan = 2)
+btn_mul.grid(row = 6, column = 0, columnspan = 2)
+btn_div.grid(row = 6, column = 1, columnspan = 2)
+
+btn_res.grid(row = 7, column = 0, columnspan = 2)
+btn_clr.grid(row = 7, column = 1, columnspan = 2)
 
 
 root.mainloop()
