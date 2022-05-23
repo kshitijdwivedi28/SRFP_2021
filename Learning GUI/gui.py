7367fb760b6dec5c3115f3d8db1e41607ef7c277
Kshitij Dwivedi <kshitijdwivedi28@gmail.com>
2022-05-24 04:28:57 +0530

    Added icon, a submit button, and built a gif, inserted it for understanding of the process

--- Learning GUI/gui.py
+++ Learning GUI/gui.py
@@ -1,13 +1,13 @@
 # importing module
 import tkinter
-from turtle import position
+from PIL import Image
 
 # creating object 
 root = tkinter.Tk()
 
 # No feature in tkinter for adjusting title, so calculating the space for positioning the title customized
 width = root.winfo_screenwidth()
-space = (" " * (int(width)//20))
+space = (" " * (int(width)//15))
 
 # Title of the Window
 root.title(space + "SRFP PROJECT BY KSHITIJ DWIVEDI")
@@ -16,7 +16,7 @@ root.title(space + "SRFP PROJECT BY KSHITIJ DWIVEDI")
 root.iconbitmap("D:\Internship SRFP\SRFP_2021\Learning GUI\icon.ico")
 
 # Creating Label Widget which puts up a text on screen
-headline = tkinter.Label(root, text = "\nSUBTITLE (SRT FILE) GENERATOR AND ALIGNER")
+headline = tkinter.Label(root, text = "\nSUBTITLE (SRT FILE) GENERATOR AND ALIGNER", font = ("Times New Roman", 20, "bold"))
 
 # Putting it up on screen and pack the window size as much as the text within, puts the text in center of the window
 headline.pack()
@@ -26,17 +26,58 @@ headline.pack()
 # myLabel.grid(row = 5, column = 5) 
 # For now using pack only.
 
+# As there's no widget for putting space in back. So,customized for creating the space between widgets
+line_space = tkinter.Label(root, text = "")
+line_space.pack()
+
+
+# Built and inserting a self created gif 
+# GIF -- START
+gif_label = tkinter.Label(root, text = "HOW IT WORKS", font = ("Times New Roman", 12, "bold"))
+gif_label.pack()
+
+
+gif_file = "D:\Internship SRFP\SRFP_2021\Learning GUI\gif_for_gui.gif"
+file_info = Image.open(gif_file)
+
+# getting the number of frames from gif, as tkinter can render one frame at a time, so will loop through frames
+# present the gif
+frames = file_info.n_frames
+
+# creating list of PhotoImage objects for each frames
+gif_frames = [tkinter.PhotoImage(file = gif_file, format = f"gif -index {i}") for i in range(frames)]
+
+gif_label = tkinter.Label(root, image="")
+gif_label.pack()
+
+count = 0
+anim = None
+
+def animation(count):
+    global anim
+    single_frame = gif_frames[count]
+
+    gif_label.configure(image = single_frame)
+    count += 1
+    
+    if count == frames:
+        count = 0
+        
+    anim = root.after(2000, lambda :animation(count))
+
+animation(count)
+# GIF -- END
+
 
 # Inputting file path from user and printing it
-file_path = tkinter.Label(root, text = "\nEnter video file path : ")
+file_path = tkinter.Label(root, text = "\nEnter Video File Path", font = ("Times New Roman", 12))
 file_path.pack()
 
 enter_data = tkinter.Entry(root, width = 50)
 enter_data.pack()
 # For default input in the 0th Input field
-enter_data.insert(0, "Enter video file path :")
+enter_data.insert(0, "Enter Video file path :")
 
-# As there's no widget for putting space in back. So,customized for creating the space between widgets
 line_space = tkinter.Label(root, text = "")
 line_space.pack()
 
@@ -69,15 +110,19 @@ def button_method():
 # Disable the button with state = disable
 # Can change text color and backgroud color in button using fg = "blue" for font, and bg = "red" also background, it also accepts hexcode of colors
 # To make buttons to do some work when clicked, define a function and call it using command = while creating button
-submit_button = tkinter.Button(root, text = "Click here to Generate Subtitles!", bg = "red", fg = "white", padx = 10, pady = 5, command = button_method, font = ("Times New Roman", 10, "bold"))
+submit_button = tkinter.Button(root, text = "Click here to Generate Subtitles!", bg = "red", fg = "white", padx = 10, pady = 5, command = button_method, font = ("Times New Roman", 12, "bold"))
 
 # Put up the button on screen
 # Using pack only for keeping the button in center
 submit_button.pack()
 
 # exit button
-exit_button = tkinter.Button(root, text = "Exit Program", padx = 10, pady = 5, command = root.quit)
+exit_button = tkinter.Button(root, text = "Exit Program", padx = 10, pady = 5, command = root.quit, font = ("Times New Roman", 12))
 exit_button.pack(side = tkinter.BOTTOM)
 
 # Hovering on the screen and keeping the program running until the window is closed
 root.mainloop()
+
+
+
+
